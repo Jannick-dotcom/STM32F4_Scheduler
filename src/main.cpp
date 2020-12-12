27fcd12fdec40ad98e7d6e335eb0a579278624a5
Jannick <jannick9743@gmail.com>
2020-12-12 18:46:52 +0100

    Changed testcode

--- src/main.cpp
+++ src/main.cpp
@@ -31,46 +31,8 @@ void tasktest()
     //}
 }
 
-void task1(void)
-{
-    StallardOSCanMessage test;
-    StallardOSCAN adcan(StallardOSCAN1, CAN500k);
-    StallardOSGPIO red(14, PORTB, Output);
-    while (1)
-    {
-        test.ID = 123;
-        test.Val = red;
-        adcan.sendMessage(&test);
-        adcan.receiveMessage(&test, 123);
-        Tasker1.delay(500);
-    }
-}
-
-void task2(void)
-{
-    StallardOSGPIO blue(7, PORTB, Output);
-    while (1)
-    {
-        blue = !blue;
-        Tasker1.delay(250);
-    }
-}
-
-void task3(void)
-{
-    StallardOSGPIO green(0, PORTB, Output);
-    while (1)
-    {
-        green = !green;
-        Tasker1.delay(125);
-    }
-}
-
 int main()
 {
-    // Tasker1.addFunction(task1, 2, 1);
-    // Tasker1.addFunction(task2, 3, 2);
-    // Tasker1.addFunction(task3, 4, 3);
     Tasker1.addFunction(tasktest, 1, 1, 30);
     Tasker1.startOS();
     while (1)
