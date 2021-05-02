3c2d7506d8f961792a65ac67681a3e6570a7875d
Jannick <jannick9743@gmail.com>
2021-05-02 03:53:45 +0300

    add saving and restoring of privilege Level for task

--- lib/OS/StallardOSContextSwitch.c
+++ lib/OS/StallardOSContextSwitch.c
@@ -176,7 +176,8 @@ __attribute__((always_inline)) void switchTask(void)
     if ((currentTask->State == RUNNING)) //Hier Task anhalten
     {
         asm("MRS r0, PSP");         //Get Process Stack Pointer
-        asm("STMDB r0!, {r4-r11}"); //Save additional not yet saved registers
+        asm("MRS r3, CONTROL");
+        asm("STMDB r0!, {r3-r11}"); //Save additional not yet saved registers
         asm("VSTMDB r0!, {s16-s31}");
         asm("MSR PSP, r0"); //Set Modified Stack pointer
         asm("MOV %0, r0" : "=r"(currentTask->Stack)); //Save Stack pointer
@@ -211,8 +212,9 @@ __attribute__((always_inline)) void switchTask(void)
     {
         asm("MOV r0, %0" : : "r"(currentTask->Stack)); //get saved Stack pointer
         asm("VLDMIA r0!, {s16-s31}");
-        asm("LDMIA r0!, {r4-r11}");   //load registers from memory
+        asm("LDMIA r0!, {r3-r11}");   //load registers from memory
         asm("MSR PSP, r0");           //set PSP
+        asm("MSR CONTROL, r3");
         currentTask->State = RUNNING; //Save state as running
         nextTask = NULL;
     }
@@ -340,7 +342,6 @@ void findNextFunction(uint32_t *minDelayT)
         if (temp->continueInMS < sysTickMillisPerInt)
         {
             temp->continueInMS = 0;
-            //temp->executable = 1; //Wenn keine Delay Zeit mehr, task auf executable setzen
         }
         else
         {
@@ -398,14 +399,14 @@ void SysTick_Handler(void) //In C Language
             pendPendSV(); //If nextTask is not this task, set the PendSV to pending
         }
     }
-// #ifdef useSystickAltering
-//     if (minDelayT >= 90 || minDelayT < 1)
-//     {
-//         minDelayT = 1;
-//     }
-//     SysTick_Config((uint32_t)(SystemCoreClock / (1000 * minDelayT))); //Set the frequency of the systick interrupt
-//     sysTickMillisPerInt = minDelayT;
-// #endif //useSystickAltering
+#ifdef useSystickAltering
+    if (minDelayT >= 90 || minDelayT < 1)
+    {
+        minDelayT = 1;
+    }
+    SysTick_Config((uint32_t)(SystemCoreClock / (1000 * minDelayT))); //Set the frequency of the systick interrupt
+    sysTickMillisPerInt = minDelayT;
+#endif //useSystickAltering
 
 #endif //contextSwitch
     
