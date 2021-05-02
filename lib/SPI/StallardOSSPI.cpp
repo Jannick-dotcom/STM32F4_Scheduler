b1960b22fedc03ff67f874af9ad78a5bf56025b1
Jannick <jannick9743@gmail.com>
2021-05-02 03:59:41 +0300

    change comment

--- lib/SPI/StallardOSSPI.cpp
+++ lib/SPI/StallardOSSPI.cpp
@@ -11,7 +11,7 @@ StallardOSSPI::StallardOSSPI(SPI_TypeDef *inst, uint32_t baud)
     handle.Init.DataSize = SPI_DATASIZE_8BIT; //SPI_DATASIZE_16BIT
     handle.Init.CLKPolarity = SPI_POLARITY_LOW; //SPI_POLARITY_HIGH
     handle.Init.CLKPhase = SPI_PHASE_1EDGE; //SPI_PHASE_2EDGE
-    handle.Init.NSS = SPI_NSS_HARD_OUTPUT;
+    handle.Init.NSS = SPI_NSS_HARD_OUTPUT; //SPI_NSS_
     handle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2; //SPI_BAUDRATEPRESCALER
     handle.Init.FirstBit = SPI_FIRSTBIT_MSB; //SPI_FIRSTBIT_LSB
     handle.Init.TIMode = SPI_TIMODE_DISABLE; //SPI_TIMODE_ENABLE
