c14c447b907e1830b3ae817856df93a81072a0b8
Jannick <jannick9743@gmail.com>
2021-09-28 20:53:01 +0200

    [OS] Update unit test

--- lib/OS/StallardOSClass.hpp
+++ lib/OS/StallardOSClass.hpp
@@ -10,57 +10,33 @@ private:
   struct function_struct *first_function_struct; //Pointer auf das erste erstellte Function struct
   uint8_t TCBsCreated;
 
-  #ifndef contextSwitch
-  //Wichtig für Overflow Handling
-  uint64_t lastScheduleTime; //Die letzte Zeit an der der Scheduler ausgeführt wurde
-  #endif
-
-public:
-  //Constructor
-  StallardOS(); //Create a Task Scheduler
-  #ifdef contextSwitch
-  // struct function_struct *addFunction(void (*function)(), uint16_t id, uint8_t prio, uint16_t refreshRate = 0);
-  struct function_struct *addFunction(void (*function)(), uint16_t id, uint8_t prio, uint32_t stackSize, uint16_t refreshRate = 0);
-  struct function_struct *addFunctionStatic(void (*function)(), uint16_t id, uint8_t prio, uint32_t *stackPtr, uint32_t stackSize, uint16_t refreshRate = 0);
-  #else
-  struct function_struct *addFunction(
-      /*Funktion*/ void (*function)(),
-      /*ID of task*/ uint16_t id,                  //The ID must be different for different tasks
-      /*Prioritaet*/ uint8_t prio,                 //Tasks with prio 0 won't be interrupted and Tasks with prio 255 will only be executed to waste Time
-      /*Executions per sec*/ uint16_t refreshRate,  //Must be bigger than 0 ! //not used by context switch
-      /*Number of execs*/ uint16_t Execcount = 0); //not used by context switch
-  #endif
-  void changeFunctionEnabled(
-      /*Funktion*/ uint16_t id,
-      /*Aktivieren oder Deaktivieren*/ bool act);
-
-private:
   struct function_struct *searchFunction(/*Funktion*/ uint16_t id); //Search a TCB that matches the provided function
   struct function_struct *searchFreeFunction(void);                 //Search a TCB that isn't used
   void createTCBs(void);                                            //create the structs for all the functions (count is "countTasks")
 
+
 public:
+  //Constructor
+  StallardOS(); //Create a Task Scheduler
+  // struct function_struct *addFunction(void (*function)(), uint16_t id, uint8_t prio, uint16_t refreshRate = 0);
+  struct function_struct *addFunction(void (*function)(), uint16_t id, uint8_t prio, uint32_t stackSize, uint16_t refreshRate = 0);
+  struct function_struct *addFunctionStatic(void (*function)(), uint16_t id, uint8_t prio, uint32_t *stackPtr, uint32_t stackSize, uint16_t refreshRate = 0);
+
+  //Setters
+  void setFunctionEnabled(/*Funktion*/ uint16_t id, /*Aktivieren oder Deaktivieren*/ bool act);
   void setFunctionPriority(/*Funktion*/ uint16_t id, /*New Priority*/ uint8_t prio);
-  // void switchPrivilegeLevel(bool status);
-  #ifndef contextSwitch
   void setFunctionFrequency(/*Funktion*/ uint16_t id, /*New execution Frequency*/ float exec_freq);
-  #endif
-  #ifdef contextSwitch
+
+  //Getters
   taskState getFunctionState(/*Funktion*/ uint16_t id);
-  // uint8_t getCPUload();
-  #endif
   uint64_t getRuntimeMs();
   uint64_t getRuntimeUs(); 
-  static void goBootloader();
-  // static void flashOverCanHandle();
-  
 
+  //Control
+  static void goBootloader();
   void startOS(void);                //RTOS Starten (preemtive Multitasking)
   static void delay(uint32_t milliseconds); //RTOS führt solange einen anderen Task aus bevor er zum jetzigen zurückspringt
   void yield();                      //RTOS sagen, dass jetzt der Code zuende ist
-  #ifndef contextSwitch
-  void schedule();                   //Execute in the Loop (cooperative Multitasking)
-  #endif
 };
 
 #endif
--- test/OS/testOS.cpp
+++ test/OS/testOS.cpp
@@ -3,6 +3,7 @@
 #include <unity.h>
 #include "StallardOS.hpp"
 StallardOS StallardOSJanniq;
+function_struct *ref;
 
 void setUp(void)
 {
@@ -13,38 +14,52 @@ void tearDown(void)
 {
 }
 
+void testfunction() {}
+
 void test_addFunction()
 {
+    uint32_t stack[500];
+    function_struct *temp;
+    temp = StallardOSJanniq.addFunction(testfunction, 1, 3, 100); //This should not return a nullptr
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, temp, "normal dynamic function add");
+    temp = StallardOSJanniq.addFunction(testfunction, 1, 1, 100); //This should return a nullptr because already given id
+    TEST_ASSERT_EQUAL_MESSAGE(nullptr, temp, "dynamic add function with same id");
+    temp = StallardOSJanniq.addFunction(testfunction, 2, 3, -1); //Try allocating a lot of stack space
+    TEST_ASSERT_EQUAL_MESSAGE(nullptr, temp, "dynamic add function with too much stack");
 
-}
-
-void test_delay()
-{
-
-}
-
-void test_yield()
-{
-
+    temp = StallardOSJanniq.addFunctionStatic(testfunction, 3, 1, stack, sizeof(stack));
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(nullptr, temp, "normal static function add");
+    ref = temp; //Save the function_struct
+    temp = StallardOSJanniq.addFunctionStatic(testfunction, 4, 1, nullptr, sizeof(stack));
+    TEST_ASSERT_EQUAL_MESSAGE(nullptr, temp, "nullptr static function add");
+    temp = StallardOSJanniq.addFunctionStatic(testfunction, 5, 1, stack, 0);
+    TEST_ASSERT_EQUAL_MESSAGE(nullptr, temp, "zero size static function add");
 }
 
 void test_functionModifiers()
 {
+    StallardOSJanniq.setFunctionEnabled(ref->id,0);
+    TEST_ASSERT_EQUAL_MESSAGE(false, ref->executable, "Disable function");
+    StallardOSJanniq.setFunctionEnabled(ref->id,1);
+    TEST_ASSERT_EQUAL_MESSAGE(true, ref->executable, "Enable function");
 
-}
+    StallardOSJanniq.setFunctionPriority(ref->id, 34);
+    TEST_ASSERT_EQUAL_MESSAGE(34, ref->priority, "Change prio");
 
-void test_timing()
-{
-    
+    StallardOSJanniq.setFunctionFrequency(ref->id, 1234);
+    TEST_ASSERT_EQUAL_MESSAGE(1234, ref->refreshRate, "change frequency");
 }
 
 int main()
 {
     HAL_Init(); // initialize the HAL library
-    StallardOS::delay(2000);
+    StallardOS::delay(5000);
+    unittest_uart_begin();
+    unittest_uart_putchar(countTasks);
     UNITY_BEGIN();
 
     RUN_TEST(test_addFunction);
+    RUN_TEST(test_functionModifiers);
 
     UNITY_END(); // stop unit testing
 
