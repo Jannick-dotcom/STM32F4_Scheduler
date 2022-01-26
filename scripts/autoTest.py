d046dac8dc698456b6c54b83a5dc6c95b097f936
Jannick <jannick9743@gmail.com>
2022-01-26 17:09:38 +0100

    [TEST] don't use environment command

--- scripts/autoTest.py
+++ scripts/autoTest.py
@@ -6,7 +6,7 @@ while True:
     result = subprocess.run(['git', 'pull'], stdout=subprocess.PIPE).stdout
     if(not str(result).__contains__("up to date")):
         with open("test/result.txt","wb") as out:
-            result = subprocess.run(['platformio', 'test', '-e', 'black_f407ve'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout
+            result = subprocess.run(['platformio', 'test'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout
             out.write(result)
             subprocess.run(['git', 'add', 'test/result.txt'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             subprocess.run(['git', 'commit', '-m', 'UnitTest result'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
