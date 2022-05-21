33d03cb62a03ad78e5f5abcaba6a636191a0237b
Wolfgang Bradfisch <w.bradfisch@gmx.de>
2022-05-21 02:34:28 +0200

    add Messages

--- lib/FaulhaberCAN/StallardOSFaulhaberMessages.hpp
+++ lib/FaulhaberCAN/StallardOSFaulhaberMessages.hpp
@@ -4,10 +4,10 @@ class StallardOSFaulhaberMessager
 {
 private:
 public:
-    static void SendPDO1Message(int node_ID);
+    static void SendPDO1Message(int node_ID, int controllword);
     static void SendPDO2Message(int node_ID, int pos);
-    static void SendPDO3Message(int node_ID);
-    static void SendPDO4Message(int node_ID);
-    static void SendSDOMessage();
+    static void SendPDO3Message(int node_ID, int vel);
+    static void SendPDO4Message(int node_ID, int tork);
+    static void SendSDOMessage(int node_ID, int cs, int index, int subindex, int data);
     static void SendNMTMessage(int node_ID, int cs);
 };
