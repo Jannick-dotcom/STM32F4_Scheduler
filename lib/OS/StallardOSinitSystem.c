23a9558e86ef3f5cf17eec82aec51eef2a184896
Jannick <jannick9743@gmail.com>
2022-06-07 02:35:27 +0200

    [OS] port init system to STM32F1

--- lib/OS/StallardOSinitSystem.c
+++ lib/OS/StallardOSinitSystem.c
@@ -3,10 +3,11 @@
 
 void StallardOS_SetSysClock(uint8_t clockspeed, oscillatorType oscType)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  //RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  // RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
 
+#ifdef STM32F4xxxx
   /* Enable Power Control clock */
   __HAL_RCC_PWR_CLK_ENABLE();
 
@@ -14,78 +15,96 @@ void StallardOS_SetSysClock(uint8_t clockspeed, oscillatorType oscType)
        clocked below the maximum system frequency, to update the voltage scaling value
        regarding system frequency refer to product datasheet. */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+#endif
 
   /* Get the Clocks configuration according to the internal RCC registers */
-  HAL_RCC_GetOscConfig(&RCC_OscInitStruct);
+  // HAL_RCC_GetOscConfig(&RCC_OscInitStruct);
 
-  /* PLL could be already configured by bootlader */
-  if (RCC_OscInitStruct.PLL.PLLState != RCC_PLL_ON)
+  if (oscType == internal)
   {
-    if(oscType == internal)
-    {
-      #ifdef STM32F4xxxx
-      /* Enable HSE oscillator and activate PLL with HSE as source */
-      RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-      RCC_OscInitStruct.HSEState = RCC_HSE_OFF; /* External xtal on OSC_IN/OSC_OUT */
-      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-      RCC_OscInitStruct.PLL.PLLM = (HSE_VALUE / 1000000);             // VCO input clock = 1 MHz (8 MHz / 8)
-      RCC_OscInitStruct.PLL.PLLN = clockspeed * 2;           // VCO output clock = 360 MHz (1 MHz * 360)
-      RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2; // PLLCLK = 180 MHz (360 MHz / 2)
-      RCC_OscInitStruct.PLL.PLLQ = 7;             //
-      #elif defined STM32F1xxxx
-      /* Enable HSE oscillator and activate PLL with HSE as source */
-      RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-      RCC_OscInitStruct.HSEState = RCC_HSE_OFF; /* External xtal on OSC_IN/OSC_OUT */
-      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-      RCC_OscInitStruct.PLL.PLLMUL = ((72000000 * 2) / HSI_VALUE);             // VCO input clock = 1 MHz (8 MHz / 8)
-      #endif
-    }
-    else if(oscType == external)
-    {
-      #ifdef STM32F4xxxx
-      /* Enable HSE oscillator and activate PLL with HSE as source */
-      RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-      RCC_OscInitStruct.HSEState = RCC_HSE_ON; /* External xtal on OSC_IN/OSC_OUT */
-      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-      RCC_OscInitStruct.PLL.PLLM = (HSE_VALUE / 1000000);             // VCO input clock = 1 MHz (8 MHz / 8)
-      RCC_OscInitStruct.PLL.PLLN = clockspeed * 2;           // VCO output clock = 360 MHz (1 MHz * 360)
-      RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2; // PLLCLK = 180 MHz (360 MHz / 2)
-      RCC_OscInitStruct.PLL.PLLQ = 7;             //
-      #elif defined STM32F1xxxx
-      /* Enable HSE oscillator and activate PLL with HSE as source */
-      RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-      RCC_OscInitStruct.HSEState = RCC_HSE_ON; /* External xtal on OSC_IN/OSC_OUT */
-      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-      RCC_OscInitStruct.PLL.PLLMUL = ((72000000 * 2) / HSE_VALUE);             // VCO input clock = 1 MHz (8 MHz / 8)
-      #endif
-    #if defined(stm32f446ze)
-    RCC_OscInitStruct.PLL.PLLR = 2;             //
-    #endif
-    }
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    {
-      while(1);
-    }
+#ifdef STM32F4xxxx
+    /* Enable HSE oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    // RCC_OscInitStruct.HSEState = RCC_HSE_OFF; /* External xtal on OSC_IN/OSC_OUT */
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = (HSE_VALUE / 1000000); // VCO input clock = 1 MHz (8 MHz / 8)
+    RCC_OscInitStruct.PLL.PLLN = clockspeed * 2;        // VCO output clock = 360 MHz (1 MHz * 360)
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;         // PLLCLK = 180 MHz (360 MHz / 2)
+    RCC_OscInitStruct.PLL.PLLQ = 2;                     //
+#elif defined STM32F1xxxx
+    /* Enable HSE oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSEState = RCC_HSE_OFF; /* External xtal on OSC_IN/OSC_OUT */
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+    RCC_OscInitStruct.PLL.PLLMUL = ((clockspeed * 2) / (HSI_VALUE / 1000000)); // VCO input clock = 1 MHz (8 MHz / 8)
+#endif
+#if defined(STM32F446ZE)
+    RCC_OscInitStruct.PLL.PLLR = 2; //
+#endif
   }
-
-  // Activate the OverDrive to reach the 180 MHz Frequency
-  // if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-  // {
-  //   while(1);
-  // }
-
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+  else if (oscType == external)
+  {
+#ifdef STM32F4xxxx
+    /* Enable HSE oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON; /* External xtal on OSC_IN/OSC_OUT */
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = (HSE_VALUE / 1000000); // VCO input clock = 1 MHz (8 MHz / 8)
+    RCC_OscInitStruct.PLL.PLLN = clockspeed * 2;        // VCO output clock = 360 MHz (1 MHz * 360)
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;         // PLLCLK = 180 MHz (360 MHz / 2)
+    RCC_OscInitStruct.PLL.PLLQ = 7;                     //
+#elif defined STM32F1xxxx
+    /* Enable HSE oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+#endif
+#if defined(STM32F446ZE)
+    RCC_OscInitStruct.PLL.PLLR = 2; //
+#endif
+  }
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    while (1)
+      ;
+  }
+// #if defined(STM32F446ZE)
+//   // Activate the OverDrive to reach the 180 MHz Frequency
+//   if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+//   {
+//     while(1);
+//   }
+// #endif
+/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+#ifdef STM32F4xxxx
   RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1; // 168 MHz
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV8;  //  21 MHz
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  //  84 MHz
+#elif defined STM32F1xxxx
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1; // 72 MHz
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  // 36 MHz
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  // 72 MHz
+#endif
+#ifdef STM32F4xxxx
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+#elif defined(STM32F1xxxx)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+#endif
   {
-    while(1);
+    while (1)
+      ;
   }
 }
