3c6409090b16c19e5b512cf4349805f4af6098cf
Christian Mayer <dev@cj-mayer.de>
2022-06-11 16:44:00 +0200

    use new CAN msg for perfMon

--- lib/OS/StallardOSTasks.hpp
+++ lib/OS/StallardOSTasks.hpp
@@ -3,7 +3,7 @@
 
 // definition in correlated .cpp file
 extern stack_T taskmainStack[256];
-extern stack_T taskPerfmonStack[256];
+extern stack_T taskPerfmonStack[2048];
 #ifdef useSFOC
     extern stack_T taskSFOCStack[256];
 #endif
