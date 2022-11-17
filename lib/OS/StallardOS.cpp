a01c7155f5b52668dd43d4934b4d7998c946757f
Jannick <jannick9743@gmail.com>
2022-11-17 17:45:53 +0100

    [OS] fix searchFunction

--- lib/OS/StallardOS.cpp
+++ lib/OS/StallardOS.cpp
@@ -596,11 +596,6 @@ void StallardOS::setFunctionPriority(/*Funktion*/ uint16_t id, uint8_t prio)
 struct function_struct *StallardOS::searchFunction(/*ID*/ uint16_t id)
 {
   volatile struct function_struct *temp = nullptr; //temporärer pointer erzeugen
-
-  if (temp == nullptr)
-  {
-    return nullptr;
-  }
   for (uint16_t i = 0; i < countTasks; i++)
   {
     if (taskArray[i].id == id)
@@ -676,7 +671,7 @@ void StallardOS::delay_us(uint16_t microseconds)
   if(microseconds >= 1000)
   {
     delay(microseconds / 1000);
-    continueTimeStamp = microseconds - ((microseconds / 1000) * 1000);
+    continueTimeStamp = microseconds - ((microseconds / 1000) * 1000);//TODO: check if this is correct
   }
   else
   {
@@ -701,8 +696,12 @@ void StallardOS::yield()
     if(currentTask->refreshRate != 0)
     {
       delay((1000 / currentTask->refreshRate) - (currentTask->lastYield - currentTask->lastStart));
-      currentTask->lastStart = StallardOSTime_getTimeMs();
     }
+    else
+    {
+      delay(1);
+    }
+    currentTask->lastStart = StallardOSTime_getTimeMs();
   }
 }
 
@@ -830,7 +829,7 @@ void StallardOS::startOS(void)
   #ifdef STM32F4xxxx
   FLASH->ACR |= (1 << FLASH_ACR_PRFTEN_Pos) | (1 << FLASH_ACR_ICEN_Pos) | (1 << FLASH_ACR_DCEN_Pos); //Enable the Flash ART-Accelerator
   #endif
-  SCB->CCR |= 1 << SCB_CCR_DIV_0_TRP_Pos | 1 << SCB_CCR_UNALIGN_TRP_Pos;
+  SCB->CCR |= 1 << SCB_CCR_DIV_0_TRP_Pos;// | 1 << SCB_CCR_UNALIGN_TRP_Pos;
   #ifdef useFPU
   #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
   SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2));  //Set the FPU to full access
@@ -847,7 +846,7 @@ void StallardOS::startOS(void)
   NVIC_EnableIRQ(SysTick_IRQn);
   NVIC_EnableIRQ(SVCall_IRQn);
   #if defined(useFPU) && (__FPU_PRESENT == 1)
-  NVIC_EnableIRQ(FPU_IRQn);
+  // NVIC_EnableIRQ(FPU_IRQn);
   #endif
   __ASM volatile("MRS R0, MSP\n"
                   "SUB R0, #200\n" //Reserve some space for Handlers (200 Byte)
