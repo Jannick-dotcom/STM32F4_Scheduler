6abcaeb93d793c452850fe0b60f277b94d78e5ec
Jannick <jannick9743@gmail.com>
2022-11-17 17:47:22 +0100

    [TIME] use uwTick directly

--- lib/TIME/StallardOSTime.cpp
+++ lib/TIME/StallardOSTime.cpp
@@ -2,13 +2,13 @@
 
 uint64_t StallardOSTime_getTimeMs()
 {
-    return HAL_GetTick();
+    return uwTick;
 }
 
 uint64_t StallardOSTime_getTimeUs()
 {
     CALL_PRIVILEGED();
-    uint64_t timestamp = (HAL_GetTick() * 1000) + SysTick->LOAD - (SysTick->VAL / (SystemCoreClock / 1000000));
+    uint64_t timestamp = (uwTick * 1000) + SysTick->LOAD - (SysTick->VAL / (SystemCoreClock / 1000000));
     CALL_UNPRIVILEGED();
     return timestamp;
 }
